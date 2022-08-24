9b366cfec0f7e894f86d18966337fe73bf59ce7a
Bugz151995 <ryan.abugao@gmail.com>
2022-08-24 12:24:12 +0800

    modified the sidebar

--- app/Views/components/breadcrumb.php
+++ app/Views/components/breadcrumb.php
@@ -1,6 +1,6 @@
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="#" class="text-decoration-none"><i class="fas fa-fw fa-home"></i> Home</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Library</li>
+    <li class="breadcrumb-item active" aria-current="page">Library</li>a
   </ol>
 </nav>
